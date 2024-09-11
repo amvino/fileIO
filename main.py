b4ecebb21de35648b141beb715edfa6e5a3d40f9
amvino <anmvin@gmail.com>
2024-09-11 11:28:24 +0300

    Добавлена функция загрузки данных из файла в формате json и отображение данных в двух списках

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from textwrap import indent
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
@@ -11,7 +10,7 @@ import os
 
 history_file = 'upload_history.json'
 
-
+# функция сохранения загрузок
 def save_history(filepath, link):
     history = []
     if os.path.exists(history_file):
@@ -37,14 +36,37 @@ def upload():
                 pyperclip.copy(link)
                 save_history(filepath, link)
                 mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена")
-
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка {e}")
 
 
+def show_history():
+    if not os.path.exists(history_file):
+        mb.showinfo("История загрузок", "История загрузок пуста")
+        return
+    else:
+        history_window = Toplevel(window)
+        history_window.title("История загрузок")
+
+    files_listbox = Listbox(history_window)
+    files_listbox.title('История загрузок')
+
+    files_listbox = Listbox(history_window, width=50, height=20)
+    files_listbox.grid(row=0, column=0, padx=(10,5), pady=10)
+
+    links_listbox = Listbox(history_window, width=50, height=20)
+    links_listbox.grid(row=0, column=1, padx=(5, 10), pady=10)
+
+    with open(history_file, 'r') as f:
+        history = json.load(f)
+        for item in history:
+            files_listbox.insert(END, item["filepath"])
+            links_listbox.insert(END, item["download_link"])
+
+
 window = Tk()
 window.title("Сохранения файлов в облаке")
-window.geometry("300x150")
+window.geometry("300x200")
 
 label = ttk.Label(text="Выберите файл для загрузки")
 label.pack(pady=10)
@@ -54,5 +76,8 @@ button.pack(pady=10)
 
 entry = ttk.Entry()
 entry.pack()
+
+history_button = ttk.Button(text="Показать историю", command=show_history)
+history_button.pack(pady=10)
 
 window.mainloop()
